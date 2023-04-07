228b7b7087cea27e6d96d038b72a3c3bb431d6e6
AboGhanem99 <awsan11111@gmail.com>
2023-04-08 04:26:35 +0500

    TopUnivers

--- src/pages/Cities/CitiesCards.tsx
+++ src/pages/Cities/CitiesCards.tsx
@@ -40,7 +40,7 @@ const CitiesCards = () => {
 
     const CitsCards: Array<JSX.Element> = arry.map(({ name, img }, i) => {
         return (
-            <div key={i} className={"w-60 h-80 flex justify-center items-center rounded-2xl shadow-lg " + state.bgColor + state.shadowColor} >
+            <div key={i} className={"w-60 h-80 flex justify-center items-center rounded-2xl shadow-lg  " + state.bgApp + state.shadowColor} >
                 <div className="flex flex-col  items-center justify-center w-48 h-full -mt-2 ">
                     <img src={img} className="pointer-events-none h-48 rounded-lg opacity-60 "></img>
                     <h1 className={"font-Cairo font-bold text-2xl self-start " + state.fontColor}>{name}</h1>
@@ -53,7 +53,7 @@ const CitiesCards = () => {
     return (
         <div className="w-full mt-8 flex justify-center ">
 
-            <div className={"w-full max-w-6xl flex justify-center gap-4 flex-wrap p-8 rounded-2xl shadow-lg mx-2 " + state.bgColor + state.shadowColor}>
+            <div className={"w-full max-w-6xl flex justify-center gap-8 flex-wrap p-8 rounded-2xl shadow-lg mx-2 " + state.bgColor + state.shadowColor}>
                 {CitsCards}
             </div>
         </div>
--- src/pages/City/TopUnivers.tsx
+++ src/pages/City/TopUnivers.tsx
@@ -1,11 +1,112 @@
 import MyComponentProps from './MyComponentProps'
+import { useMyContext } from "../../State/MyContext"
 
 const TopUnivers: React.FC<MyComponentProps> = ({ name }) => {
+  const { state } = useMyContext()
+
+  const MoscowUniversities = [
+    {
+      name: "",
+      img: "",
+      info: "",
+      link: "",
+    }
+  ]
+  const StPetersburgUniversities = [
+    {
+      name: "",
+      img: "",
+      info: "",
+      link: "",
+    },
+  ]
+  const UfaUniversities = [
+    {
+      name: "",
+      img: "",
+      info: "",
+      link: "",
+    },
+  ]
+  const KazanUniversities = [
+    {
+      name: "",
+      img: "",
+      info: "",
+      link: "",
+    },
+  ]
+  const RostovUniversities = [
+    {
+      name: "",
+      img: "",
+      info: "",
+      link: "",
+    },
+  ]
+  const YekaterinburgUniversities = [
+    {
+      name: "",
+      img: "",
+      info: "",
+      link: "",
+    },
+  ]
+
+
+  let CityUniversities: Array<any> = []
+  switch (name) {
+    case "Moscow":
+      CityUniversities = MoscowUniversities
+      break
+    case "St Petersburg":
+      CityUniversities = StPetersburgUniversities
+      break
+    case "Ufa":
+      CityUniversities = UfaUniversities
+      break
+    case "Kazan":
+      CityUniversities = KazanUniversities
+      break
+    case "Rostov":
+      CityUniversities = RostovUniversities
+      break
+    case "Yekaterinburg":
+      CityUniversities = YekaterinburgUniversities
+      break
+  }
+  const CityUniversitiesHtml: Array<JSX.Element> = CityUniversities.map(({ name, img, info, link }, i) => {
     return (
-        <div>
-          topUnivers - top 5 university in this city (image of the university and small description about this university).
-        </div> 
-    );
+      <div key={i} className={'w-full max-w-6xl h-96 rounded-br-3xl shadow-lg py-8 text-right flex mt-28  ' + state.bgApp + state.shadowColor + (i === 0 || i % 2 === 0 ? "flex-row-reverse" : " ")}>
+        <div className='w-2/3'>
+          <p className={'font-Cairo text-sm md:text-base text-right mx-8 ' + state.fontColor}>{info}</p>
+          <a className='font-Cairo text-sm md:text-base text-center mx-8 text-[#48cae4] font-bold underline ' target="_blank" href={link}>موقح الجامعة</a>
+        </div>
+        <div >
+          <img src={img} className={"pointer-events-none -mt-24 h-96 w-96 rounded-lg shadow-lg  " + state.shadowColor}></img>
+          <h1 className='font-Cairo text-sm md:text-2xl font-extrabold -mt-12 mx-8 text-[#48cae4]'>{name}</h1>
+        </div>
+      </div>
+    )
+  })
+
+
+  return (
+    <div>
+      <div className={'w-full mt-8 flex justify-center '}>
+        <div className={"w-full max-w-6xl p-8 rounded-2xl shadow-lg flex flex-col justify-center items-center mx-2  " + state.bgColor + state.shadowColor}>
+
+          <div className='flex justify-center mb-2'>
+            <div className={'h-px w-6 mt-4 mr-1 ' + (state.dark ? "bg-[#FFFFFF] " : "bg-[#214966] ")} ></div>
+            <h1 className={'font-Cairo md:text-xl font-extrabold text-center ' + state.fontColor}>اشهر الجامعات في المدينة</h1>
+            <div className={'h-px w-6 mt-4 mr-1 ' + (state.dark ? "bg-[#FFFFFF] " : "bg-[#214966] ")} ></div>
+          </div>
+
+          {CityUniversitiesHtml}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TopUnivers
